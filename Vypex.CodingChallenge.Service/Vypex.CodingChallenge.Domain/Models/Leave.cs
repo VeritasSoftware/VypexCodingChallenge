212ae8b931044aaccbc05dde9167fee71ea60b60
Shantanu Negi <shantanu_negi@hotmail.com>
2025-05-16 15:44:06 +1000

    added leave model & repository initial.

---
+++ Vypex.CodingChallenge.Service/Vypex.CodingChallenge.Domain/Models/Leave.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vypex.CodingChallenge.Domain.Models
+{
+    public class Leave : IValidatableObject
+    {
+        public Guid Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (EndDate <= StartDate)
+            {
+                yield return new ValidationResult("End date must be after Start date.", new[] { nameof(StartDate), nameof(EndDate) });
+            }            
+        }
+    }
+}
---
+++ Vypex.CodingChallenge.Service/Vypex.CodingChallenge.Infrastructure/Repositories/LeaveRepository.cs
@@ -0,0 +1,30 @@
+﻿using Vypex.CodingChallenge.Domain.Models;
+using Vypex.CodingChallenge.Infrastructure.Data;
+
+namespace Vypex.CodingChallenge.Infrastructure.Repositories
+{
+    public class LeaveRepository : IRepository<Leave>
+    {
+        private readonly CodingChallengeContext _context;
+
+        public LeaveRepository(CodingChallengeContext context) 
+        {
+            _context = context;
+        }
+
+        public Task<Leave> AddAsync(Leave entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Leave> UpdateAsync(Leave entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
