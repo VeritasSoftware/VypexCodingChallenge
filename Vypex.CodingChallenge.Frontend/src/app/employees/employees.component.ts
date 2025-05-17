87a59032c5ab922b6b364e09c5c31e9151fa2b68
Shantanu Negi <shantanu_negi@hotmail.com>
2025-05-18 08:54:27 +1000

    added error handling for potential api errors to employees component in UI.

--- Vypex.CodingChallenge.Frontend/src/app/employees/employees.component.ts
+++ Vypex.CodingChallenge.Frontend/src/app/employees/employees.component.ts
@@ -1,8 +1,11 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { NzAlertComponent } from 'ng-zorro-antd/alert';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { BehaviorSubject, catchError, Observable } from 'rxjs';
 import { EditEmployeeModal } from './edit-employee/edit-employee.modal';
+import { Employee } from './models';
 import { EmployeeApiService } from './services/employee-api.service';
 
 @Component({
@@ -10,6 +13,7 @@ import { EmployeeApiService } from './services/employee-api.service';
   imports: [
     NzTableModule,
     NzButtonComponent,
+    NzAlertComponent,
     AsyncPipe
   ],
   providers: [
@@ -18,11 +22,28 @@ import { EmployeeApiService } from './services/employee-api.service';
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss'
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit {
   private readonly employeeApiService = inject(EmployeeApiService);
   private readonly editEmployeeModal = inject(EditEmployeeModal);
 
-  public readonly employees$ = this.employeeApiService.getEmployees();
+  public employees$: Observable<Employee[]> = new Observable<Employee[]>();
+  public error$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+  public ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.employees$ = new Observable<Employee[]>();
+    this.error$.next(null); // Reset error state.
+    this.employees$ = this.employeeApiService.getEmployees().pipe(
+      catchError((e) => {
+        console.error('Error loading employees', e.message);
+        this.error$.next(e);
+        return new Observable<Employee[]>(observer => observer.next([]));
+      })
+    );
+  }
 
   public edit(employeeId: number) {
     this.editEmployeeModal.open({ id: employeeId })
